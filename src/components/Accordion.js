2a1c07d39205c51df36956816e7b8ffed7824664
Har Dhillon <har7173@gmail.com>
2021-03-04 18:36:43 +0400

    adding semantic styling, shirnking and expanding the accordion by adding or removing active class

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Accordion = () => {
-  return <h1>Accordion</h1>
-};
+const Accordion = ({ items }) => {
+
+  // first item is the state, second is the function we call to change it
+  const [activeIndex, SetActiveIndex] = useState(null) 
+
+  const onTitleClick = (index) => {
+    SetActiveIndex(index)
+  };
+ 
+  const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : ''
+
+    return (
+      // rather than div we return a react fragment to get rid of an extra div (for semantic ui styling)
+      <React.Fragment key={item.title}>
+        <div
+        className={`title ${active}`}
+        // arrow function so that it does not run on render, but ONLY on click
+        onClick={() => onTitleClick(index)}
+        >
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    )
+  })
+
+  return <div className="ui styled accordion">
+    {renderedItems}
+  </div>
+}
 
 export default Accordion;
