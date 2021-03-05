15b70c4c3db339665a67f4b4293baa3489a5c585
Har Dhillon <har7173@gmail.com>
2021-03-05 14:52:54 +0400

    using useEffect everytime term changes and adding onChange function to input

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Search = () => {
-  return <h1>Search</h1>
+  const [term, setTerm] = useState('')
+
+  // 2nd argument is either 1) nothing, 2) empty array, 3) array with something
+  useEffect(() => {
+    console.log('I run everytime term updates')
+  }, [term])
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+          className="input"
+          value={term}
+          onChange={e => setTerm(e.target.value )} 
+          />
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Search;
