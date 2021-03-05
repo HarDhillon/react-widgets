76f58aeb96dcb869beb0e423c91061e5ded0ed5e
Har Dhillon <har7173@gmail.com>
2021-03-05 15:12:43 +0400

    adding axios and using it to search the wiki api

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 const Search = () => {
   const [term, setTerm] = useState('')
 
   // 2nd argument is either 1) nothing, 2) empty array, 3) array with something
   useEffect(() => {
-    console.log('I run everytime term updates')
+    const search = async () => {
+        await axios.get("https://en.wikipedia.org/w/api.php", {
+          params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: term
+          }
+        });
+     };
+
+     search()
   }, [term])
 
   return (
