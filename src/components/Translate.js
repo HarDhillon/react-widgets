3b22da7029fb53481e348e26b40b7391e0502e54
Har Dhillon <har7173@gmail.com>
2021-03-06 15:50:00 +0400

    using axios to post the request to google api and displaying result by using state

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import Dropdown from './DropDown';
 import Convert from './Convert';
 
-// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
-
 const options = [
   {
     label: "Afrikaans",
